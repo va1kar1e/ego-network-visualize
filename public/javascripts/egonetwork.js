015f004b3d95d855e428ec01687d16390b6356e3
Siwanont Sittinam <siwanont1@gmail.com>
2018-08-27 18:25:57 +0700

    Node Node

--- public/javascripts/egonetwork.js
+++ public/javascripts/egonetwork.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var path = {
-        "user_id": "juthamas_rat",
-        "json_path": "friendinterest_split_5/",
+        "user_id": "t_athicha",
+        "json_path": "friendinterest_split_0/",
     }
     // $("button#userrun").click(function () {
     //     egograph(path);
@@ -57,27 +57,31 @@ function egograph(path) {
         var link = container.selectAll("line").data(graph.links)
             .enter().append("line")
             .attr("class", "link")
-            .style("stroke-width", 1)
+            .attr("group", function(d) { return d.group })
+            .style("stroke-width", 1).style("stroke-opacity", 0.5)
             .style("stroke", function (d) { return color_link(d); })
             .style("cursor", "default");
 
         var node = container.selectAll("circle").data(graph.nodes)
             .enter().append("circle")
+            .attr("group", function (d) { return d.group; })
+            .attr("name", function (d) { return d.name; })
             .attr("r", function (d) {
                 if (d.group === "Main") return "10";
                 else return "6";
             })
+            .style("opacity", 1).style("stroke-width", 0).style("stroke", "red")
             .attr("fill", function (d) { return color_node(d); })
-            // .on("click", function (d) {
-            //     mouseout_node();
-            //     mouseover_node(d);
-            // });
+            .on("click", function (d) {
+                mouseout_node();
+                mouseover_node(d);
+            });
 
         var label = container.selectAll("text").data(graph.nodes)
             .enter().append("text")
             .attr("class", "node_label")
             .attr("font-size", 2)
-            .style("fill-opacity", 0.1)
+            .style("fill-opacity", 0.2)
             .style("fill", "#213451")
             .text(function (d) { return d.name; });
 
@@ -104,8 +108,7 @@ function egograph(path) {
                 .attr("y2", function (d) { return d.target.y; });
         }
 
-        var mouseover_node = function (z) {
-
+        function mouseover_node(z) {
             var neighbors = {};
             neighbors[z.index] = true;
 
@@ -121,36 +124,99 @@ function egograph(path) {
                 }
             })
                 .style("stroke-opacity", 1)
-                .style("stroke-width", 1)
-            // .style("stroke", "#FF0000");
+                .style("stroke-width", 2)
 
             node.filter(function (d) { return neighbors[d.index] })
-                .style("stroke-width", 3)
+                .style("stroke-width", 2)
                 .style("opacity", 1);
 
             node.filter(function (d) { return !neighbors[d.index] })
-                .style("opacity", 0.5);
+                .style("stroke-width", 0)
+                .style("opacity", 0.3);
 
             label.filter(function (d) { return !neighbors[d.index] })
-                .style("fill-opacity", 0.5);
+                .attr("font-size", 1)
+                .style("fill-opacity", 0.2);
 
             label.filter(function (d) { return neighbors[d.index] })
                 .attr("font-size", 3)
-                .style("fill-opacity", 1)
+                .style("fill-opacity", 1);
 
         };
 
-        var mouseout_node = function () {
-            link.style("stroke-opacity", 0.5)
-                .style("stroke-width", 1)
-            // .style("stroke", "#999999");
+        function mouseout_node() {
+            link.style("stroke-opacity", 1)
+                .style("stroke-width", 0.5)
 
-            node.style("stroke-width", 1)
+            node.style("stroke-width", 0)
                 .style("opacity", 1);
 
             label.attr("font-size", 2)
-                .style("fill-opacity", 0.1)
+                .style("fill-opacity", 0.2)
         };
 
+        d3.select("#group_show").html(getDetail(graph.category));
+        d3.select("#clear_input").on("click", function (d) {
+            mouseout_node();
+        });
+
+        function getDetail(n) {
+            d3.select("#group_show").style('display', 'grid');
+            var info = '<p style="margin-bottom: 0.5em;">   Category </p>';
+            n.forEach(function (value, index) {
+                info += '<a href="#" onclick="groupover_node(\'' + value + '\');">' + (index + 1) + '.' + value + '</a> '
+            });
+            info += '';
+            return info;
+        }
     });
+}
+
+function groupover_node(z) {
+    var nodes = d3.select("#egonetwork").selectAll("circle");
+        links = d3.select("#egonetwork").selectAll("line");
+        labels = d3.select("#egonetwork").selectAll("text");
+
+    links.style("stroke-opacity", 1)
+        .style("stroke-width", 0.5)
+
+    nodes.style("stroke-width", 0)
+        .style("opacity", 1);
+
+    labels.attr("font-size", 2)
+        .style("fill-opacity", 0.2)
+
+    var neighbors = {};
+
+    links.filter(function (d) {
+        if (d.group == z) {
+            neighbors[d.source.id] = true
+            neighbors[d.target.id] = true
+            return true
+        } else {
+            return false
+        }
+    })
+        .style("stroke-opacity", 1)
+        .style("stroke-width", 2)
+
+    nodes.filter(function (d) {
+        if (d.group == "Main") {
+            return true
+        } else {
+            return neighbors[d.id]
+        }
+    })
+        .style("stroke-width", 2)
+        .style("opacity", 1);
+
+    labels.filter(function (d) {
+        if (d.group == "Main") {
+            return true
+        } else {
+            return neighbors[d.id]
+        }
+    })
+        .attr("font-size", 3)
+        .style("fill-opacity", 1);
 }
